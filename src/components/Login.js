e96fa35f9ca91c8445f27bc297e283a4535168c7
Sajid <mohamedsajid450@gmail.com>
2025-07-04 16:15:53 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -64,7 +64,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://yh43cj-5000.csb.app/api/login", {
+      const res = await fetch("https://smart-tasker-frontend-dlpo.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
