f61dd433a964f7f8fc88178ae51e46d34b5418fa
Sajid <mohamedsajid450@gmail.com>
2025-07-04 15:58:51 +0530

    Update Login.js with cors error

--- src/components/Login.js
+++ src/components/Login.js
@@ -64,11 +64,14 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://smarttaskerbackend-production.up.railway.app/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
+      const res = await fetch(
+        "https://smarttaskerbackend-production.up.railway.app/api/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       const data = await res.json();
 
